d6da7546f74ff0a4c6277ea1a31642f6ed3e9afa
YoanDieu <yoan.dieu@gmail.com>
2016-05-25 15:00:27 +0200

    made angular default http config and interceptor

---
+++ chapter6/public/http-default.js
@@ -0,0 +1,31 @@
+angular.module("notesApp", [])
+.controller("LoginCtrl", ["$http", function($http){
+  var self = this;
+  self.user = {};
+  self.message = "Please login";
+  self.login = function(){
+    $http.post("http://localhost:8000/api/login", self.user)
+    .then(function(resp){
+      self.message = resp.data.msg;
+    });
+  };
+}])
+.config(["$httpProvider", function($httpProvider){
+  $httpProvider.defaults.transformRequest.push(function(data){
+    var requestStr;
+    if(data){
+      data = JSON.parse(data);
+      for(var key in data){
+        if(requestStr){
+          requestStr += '&' + key + '=' + data[key];
+        }else{
+          requestStr = key + '=' + data[key];
+        }
+      }
+    }
+    return requestStr;
+  });
+
+  $httpProvider.defaults.headers.post['Content-Type'] =
+    'application/x-www-urlencoded';
+}])
---
+++ chapter6/public/logging-interceptors.js
@@ -0,0 +1,47 @@
+angular.module('notesApp', [])
+.controller("MainCtrl", ['$http', function($http){
+  var self = this;
+  self.items = [];
+  self.newTodo = {};
+  var fetchTodos = function(){
+    return $http.get('http://localhost:8000/api/note').then(function(response){
+      self.items = response.data
+    }, function(errResponse){
+      console.log('Error while fetching notes');
+    });
+  };
+
+  fetchTodos();
+
+  self.add = function(){
+    $http.post('http://localhost:8000/api/note', self.newTodo)
+    .then(fetchTodos)
+    .then(function(response){
+      self.newTodo = {};
+    });
+  };
+
+}])
+.factory('MyLoggingInterceptor', ["$q", function($q){
+  return {
+    request: function(config){
+      console.log('Request made with', config);
+      return config;
+    },
+    requestError: function(rejection){
+      console.log('Request error du to ', rejection);
+      return $q.reject(rejection);
+    },
+    response: function(response){
+     console.log("Response from server ", response);
+     return response || $q.when(response);
+   },
+   responseError: function(rejection){
+     console.log('Error in response', rejection);
+     return $q.reject(rejection);
+   }
+ };
+}])
+.config(["$httpProvider", function($httpProvider){
+  $httpProvider.interceptors.push('MyLoggingInterceptor');
+}]);
